048d550e167c3a65886c664adc68cc53c2fc286f
GustavoFringGaming <113339801+GustavoFringGaming@users.noreply.github.com>
2023-11-30 16:02:04 +0100

    update fetch script

---
+++ development/1-fetch-projects.sh
@@ -0,0 +1,19 @@
+@echo off
+echo "STEP 1- Cloning projects..."
+
+export GIT_PREFIX=https://github.com/twit2
+
+if [ "%1" == "--ssh" ]; then
+    echo Fetching projects using SSH...
+    GIT_PREFIX=git@github.com:twit2
+else;
+    echo Fetching projects using HTTPS...
+fi;
+
+git clone $GIT_PREFIX/std-library.git projects/std-library
+git clone $GIT_PREFIX/svc-users.git projects/svc-users
+git clone $GIT_PREFIX/svc-auth.git projects/svc-auth
+git clone $GIT_PREFIX/spec.git projects/spec
+git clone $GIT_PREFIX/frontend.git projects/frontend
+git clone $GIT_PREFIX/svc-posts.git projects/svc-posts
+
