e9d27fedc0696bb0bb6cebc8de2969434f347206
GustavoFringGaming <113339801+GustavoFringGaming@users.noreply.github.com>
2023-11-30 16:20:44 +0100

    update development script

--- development/1-fetch-projects.sh
+++ development/1-fetch-projects.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+echo "T2 Development script"
 echo "STEP 1- Cloning projects..."
 
 export GIT_PREFIX=https://github.com/twit2
+export PROJECTS=(std-library svc-users svc-auth svc-posts frontend)
 
 if [ "%1" == "--ssh" ]; then
     echo Fetching projects using SSH...
@@ -10,10 +12,15 @@ else
     echo Fetching projects using HTTPS...
 fi;
 
-git clone $GIT_PREFIX/std-library.git projects/std-library
-git clone $GIT_PREFIX/svc-users.git projects/svc-users
-git clone $GIT_PREFIX/svc-auth.git projects/svc-auth
-#git clone $GIT_PREFIX/spec.git projects/spec
-git clone $GIT_PREFIX/frontend.git projects/frontend
-git clone $GIT_PREFIX/svc-posts.git projects/svc-posts
-
+# Process project pulls
+for t in ${PROJECTS[@]}; do
+    if [ -d projects/$t ]; then
+        echo "Project '$t' exists, syncing changes..."
+        cd projects/$t
+        git pull
+        cd ..
+    else
+        echo "Project '$t' has not been cloned - cloning now..."
+        git clone $GIT_PREFIX/$t.git projects/$t
+    fi;
+done
