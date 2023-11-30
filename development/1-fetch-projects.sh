ec5aa5a073545d60da55bcd2e8e8ea99a77b1f02
GustavoFringGaming <113339801+GustavoFringGaming@users.noreply.github.com>
2023-11-30 16:16:19 +0100

    exclude spec for now

--- development/1-fetch-projects.sh
+++ development/1-fetch-projects.sh
@@ -13,7 +13,7 @@ fi;
 git clone $GIT_PREFIX/std-library.git projects/std-library
 git clone $GIT_PREFIX/svc-users.git projects/svc-users
 git clone $GIT_PREFIX/svc-auth.git projects/svc-auth
-git clone $GIT_PREFIX/spec.git projects/spec
+#git clone $GIT_PREFIX/spec.git projects/spec
 git clone $GIT_PREFIX/frontend.git projects/frontend
 git clone $GIT_PREFIX/svc-posts.git projects/svc-posts
 
